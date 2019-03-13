9228120a965a8c935e8897c5c470d93c2d872ffb
paulina <paula.szymaszek@gmail.com>
2019-03-13 11:36:34 +0100

    improve aggressive agent

--- hearthbreaker/agents/aggressive_agent.py
+++ hearthbreaker/agents/aggressive_agent.py
@@ -5,7 +5,6 @@ from hearthbreaker.agents.basic_agents import Agent
 # sprawdza inne mozliwości
 # wybrać wszytskie karty ktorymi można zadac obrazenia w tej rundzie
 # użyć tej ktora da najwieksze obrazenie
- 
 # atakuje albo stronnikiem albo karta 
 
 # 1) najpierw po wszyst. kartach, jakie można użyć, to je użyć
@@ -16,24 +15,35 @@ class AggressiveAgent(Agent):
         return [True, True, True, True]
 
     def check_opponent_life(self, player):
-        print (player.game.other_player.hero.health)
+        print ("Health of opponent's hero: {}".format(player.game.other_player.hero.health))
+    
+    def attack_with_hero(self,player):
+        if player.hero.can_attack():
+            print('BEFORE ATTACK WITH HERO')
+            self.check_opponent_life(player)
+            player.hero.attack()
+            self.check_opponent_life(player)
+            print('AFTER ATTACK WITH HERO')
+        print('HERO CANNOT ATTACK')
 
+    # player atakuje wszystkimi swoimi stronnikami, zaczyna od ataku na other_player.hero
     def attack_with_all_possible_minions(self,player):
         for minion in player.minions:
             if minion.can_attack():
-                print('BEFORE ATTACK')
+                print('BEFORE ATTACK WITH MINIONS')
                 self.check_opponent_life(player)
                 minion.attack()
                 self.check_opponent_life(player)
-                print('AFTER ATTACK')
-    
+                print('AFTER ATTACK WITH MINIONS')
+            print('NONE MINION CAN ATTACK')
+    # player używa wszystkie karty jakie może użyć, czyli dopóki starczy mu many
     def use_all_possible_cards(self, player):
         card_costs = [card.mana for card in player.hand]
-        # player używa wszystkie karty jakie może użyć
+        print("Card costs: {}".format(card_costs))
         done_something = True
+        
         while done_something:
             print("Current mana: {}".format(player.mana))
-            print("Card costs: {}".format(card_costs))
             done_something = False
             for card in player.hand:
                 if card.can_use(player, player.game):
@@ -42,12 +52,17 @@ class AggressiveAgent(Agent):
                     done_something = True
                     break
 
+    # player używa power swojego hero
+    def use_hero_power(self, player):
+        if player.hero.power.can_use():
+                player.hero.power.use()
+
     def do_turn(self, player):
         print("START TURN")
         self.use_all_possible_cards(player)
         self.attack_with_all_possible_minions(player)
-        
-        
+        self.attack_with_hero(player)
+        self.use_hero_power(player)
         print("END TURN")
            
 
