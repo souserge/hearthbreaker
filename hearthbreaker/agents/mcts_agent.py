225618fee8df3c200709513fff0090de2da1d87b
souserge <15277406+souserge@users.noreply.github.com>
2019-04-04 10:26:02 +0200

    Correct syntax and add _start_turn()

--- hearthbreaker/agents/mcts_agent.py
+++ hearthbreaker/agents/mcts_agent.py
@@ -1,22 +1,26 @@
 from hearthbreaker.agents.basic_agents import Agent, DoNothingAgent
-from itertools import combinations 
+from itertools import combinations
 import functools
 import operator
 import random
 import math
 from hearthbreaker.agents.basic_agents import RandomAgent, DoNothingAgent
 
+
 def find_minion(m, ms):
-    filtered_minions = list(filter(lambda x: x.key==m.key, ms)) 
-    if len(filtered_minions)<1:
-        raise Exception('No minion: {}\n found in: {}\n key: {}, keys: {}\n\n'.format(m, ms, m.key, list(map(lambda x: x.key, ms))))
+    filtered_minions = list(filter(lambda x: x.key == m.key, ms))
+    if len(filtered_minions) < 1:
+        raise Exception('No minion: {}\n found in: {}\n key: {}, keys: {}\n\n'.format(
+            m, ms, m.key, list(map(lambda x: x.key, ms))))
     return filtered_minions[0]
+
 
 def find_target(t, ts, hero):
     if t.is_hero():
         return hero
     else:
         return find_minion(t, ts)
+
 
 def attack_target(minion, target, game):
     m = find_minion(minion, game.current_player.minions)
@@ -25,14 +29,14 @@ def attack_target(minion, target, game):
 
 
 def play_move(game, chosen_move):
-    cards, attacks = chosen_move
+    game._start_turn()
 
-    if len(cards) > 0:
-        for card in cards:
-            game.play_card(card)
-    if len(attacks) > 0:
-        for (minion, target) in attacks:
-            attack_target(minion, target, game)
+    cards, attacks = chosen_move
+    for card in cards:
+        game.play_card(card)
+
+    for (minion, target) in attacks:
+        attack_target(minion, target, game)
 
     game._end_turn()
 
@@ -44,6 +48,7 @@ def get_minions_to_use(game):
             minions_to_use.append(minion)
     return minions_to_use
 
+
 def get_inner_tree(game):
     attack_sequences = []
     minions_use = get_minions_to_use(game)
@@ -51,14 +56,15 @@ def get_inner_tree(game):
         targets = minion.get_targets()
         for target in targets:
             attack = (minion, target)
-            attack_sequences.append([attack]) 
+            attack_sequences.append([attack])
             game_copy = game.copy()
             attack_target(minion, target, game_copy)
             a = get_inner_tree(game_copy)
-            new_a = list(map(lambda x: [attack]+x,a))
+            new_a = list(map(lambda x: [attack] + x, a))
             attack_sequences += new_a
-    
+
     return attack_sequences
+
 
 class GameState:
     """ A state of the game, i.e. the game board. These are the only functions which are
@@ -67,10 +73,11 @@ class GameState:
         GetRandomMove() function to generate a random move during rollout.
         By convention the players are numbered 1 and 2.
     """
+
     def __init__(self, game):
         self.game = game
-        self.playerJustMoved = game.current_player # At the root pretend the player just moved is player 2 - player 1 has the first move
-        
+        self.playerJustMoved = game.other_player  # At the root pretend the player just moved is player 2 - player 1 has the first move
+
     def clone(self):
         return GameState(self.game.copy())
 
@@ -81,7 +88,6 @@ class GameState:
         play_move(self.game, move)
         self.playerJustMoved = self.game.current_player
 
-        
     def get_moves(self):
         """ Get all possible moves from this state.
         """
@@ -89,14 +95,14 @@ class GameState:
         opponent = self.game.other_player
         if player.hero.dead or opponent.hero.dead:
             return []
+
         cards = player.hand
         possible_cards_to_play = list(filter(lambda x: x.mana <= player.mana, cards))
-        # get all combinations of cards play (order doesn't matter): 
-        a = list(filter(lambda x: x.mana_cost() <= player.mana, cards))
+        # get all combinations of cards play (order doesn't matter):
         cards_combinations = []
-        for r in range(len(possible_cards_to_play)+1):
-	        cards_combinations.extend(list(combinations(possible_cards_to_play, r)))
-
+        for r in range(len(possible_cards_to_play) + 1):
+            combs = list(map(lambda x: list(x), combinations(possible_cards_to_play, r)))
+            cards_combinations.extend(combs)
 
         cards_combinations = list(
             filter(lambda xs: sum([x.mana_cost() for x in xs]) <= player.mana, cards_combinations))  # + [[]]
@@ -104,7 +110,7 @@ class GameState:
         # get all combinations of attacks (order matters):
         attack_sequences = get_inner_tree(self.game) + [[]]
 
-        seq = map(lambda cc: list(map(lambda aseq: (cc,aseq), attack_sequences)), cards_combinations)
+        seq = map(lambda cc: list(map(lambda aseq: (cc, aseq), attack_sequences)), cards_combinations)
         # [[(), ()],[(), ()]] => [(), (), (), ()]
         all_possible_moves = functools.reduce(operator.add, seq, [])
 
@@ -118,6 +124,7 @@ class GameState:
     def __repr__(self):
         pass
 
+
 class MCTSAgent(DoNothingAgent):
     def __init__(self, depth=100):
         super().__init__()
@@ -125,39 +132,42 @@ class MCTSAgent(DoNothingAgent):
 
     def do_turn(self, player):
         state = GameState(player.game)
-        move = UCT(rootstate = state, itermax = self.depth, verbose = False)
+        move = UCT(rootstate=state, itermax=self.depth, verbose=False)
         play_move(player.game, move)
+
 
 class Node:
     """ A node in the game tree. Note wins is always from the viewpoint of playerJustMoved.
         Crashes if state not specified.
     """
-    def __init__(self, move = None, parent = None, state = None):
-        self.move = move # the move that got us to this node - "None" for the root node
-        self.parentNode = parent # "None" for the root node
+
+    def __init__(self, move=None, parent=None, state=None):
+        self.move = move  # the move that got us to this node - "None" for the root node
+        self.parentNode = parent  # "None" for the root node
         self.childNodes = []
         self.wins = 0
         self.visits = 0
-        self.untriedMoves = state.get_moves() # future child nodes
-        self.playerJustMoved = state.playerJustMoved # the only part of the state that the Node needs later
-        
+        self.untriedMoves = state.get_moves()  # future child nodes
+        self.playerJustMoved = state.playerJustMoved  # the only part of the state that the Node needs later
+
     def uct_select_child(self):
         """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
             lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
             exploration versus exploitation.
         """
-        s = sorted(self.childNodes, key = lambda c: c.wins/c.visits + math.sqrt(2*math.log(self.visits)/c.visits))[-1]
+        s = sorted(self.childNodes, key=lambda c: c.wins / c.visits
+                   + math.sqrt(2 * math.log(self.visits) / c.visits))[-1]
         return s
-    
+
     def add_child(self, m, s):
         """ Remove m from untriedMoves and add a new child node for this move.
             Return the added child node
         """
-        n = Node(move = m, parent = self, state = s)
+        n = Node(move=m, parent=self, state=s)
         self.untriedMoves.remove(m)
         self.childNodes.append(n)
         return n
-    
+
     def update(self, result):
         """ update this node - one additional visit and result additional wins. result must be from the viewpoint of playerJustmoved.
         """
@@ -171,19 +181,19 @@ class Node:
     def tree_to_string(self, indent):
         s = self.indent_string(indent) + str(self)
         for c in self.childNodes:
-             s += c.tree_to_string(indent + 1)
+            s += c.tree_to_string(indent + 1)
         return s
 
     def indent_string(self, indent):
         s = "\n"
-        for i in range (1,indent+1):
+        for i in range(1, indent + 1):
             s += "| "
         return s
 
     def children_to_string(self):
         s = ""
         for c in self.childNodes:
-             s += str(c) + "\n"
+            s += str(c) + "\n"
         return s
 
 
@@ -220,4 +230,4 @@ def UCT(rootstate, itermax, verbose=False):
         print(rootnode.tree_to_string(0))
         print(rootnode.children_to_string())
 
-    return sorted(rootnode.childNodes, key=lambda c: c.visits)[-1].move # return the move that was most visited
+    return sorted(rootnode.childNodes, key=lambda c: c.visits)[-1].move  # return the move that was most visited
